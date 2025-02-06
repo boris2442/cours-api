19bf8b6022ed132c10e3d8d3713265940329bb83
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-06 13:50:15 +0100

    debugage

--- user-app/scriptApp.js
+++ user-app/scriptApp.js
@@ -11,29 +11,25 @@
 
 
 
-const userData=[];
+let userData=[];
 const fetchUser= async()=>{
- await   fetch("https://randomuser.me/api/?results=24").then((res)=>res.json()).then((data)=>{
-    console.log(data.results)
-//    userData1=data.results
-// //   console.log(data.results.)
-})
-// console.log(userData1)
+ await   fetch("https://randomuser.me/api/?results=24")
+ .then((res)=>res.json())
+ .then((data)=>  userData=data.results)
+console.log(userData)
 
 }
-const userDisplay=()=>{
-    fetchUser();
+const userDisplay=async ()=>{
+  await  fetchUser();
 document.body.innerHTML=userData.map(
-    (user)=>{
-        return `
-        <div>
-            <h2>${user.name.first} ${user.name.last}</h2>
-            <p>${user.email}</p>
-            <img src="${user.picture.medium}" alt="${user.name.first} ${user.name.last}">
-        </div>
+    (user)=>
+       
         `
-    }
-)
+        <h2>${user.name.first}</h2>
+        `
+
+   
+).join('\n')
 }
 
 userDisplay();
